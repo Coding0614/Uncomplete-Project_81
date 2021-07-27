558ccc172afeb2252ee60c4ea5af6e9e822dc3bc
Coding0614 <77206632+Coding0614@users.noreply.github.com>
2021-07-27 19:44:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+canvas = document.getElementById("Olympic_Canvas");
+ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 2;
+ctx.rect(150,143,430,200);
+ctx.stroke();
